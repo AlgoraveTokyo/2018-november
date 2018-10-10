87f34df2cb21583a5030b727e244d2984e03887a
moxuse <moxuse@gmail.com>
2018-10-10 22:12:30 +0900

    added information.txt

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var bg_el = document.querySelector('.bg-img');
+var containerEl = document.getElementById("container");
 
 window.addEventListener("scroll", function() {
-  var scrollH = document.documentElement.scrollHeight || document.body.scrollHeight;
-  var scrollV = scrollH　- window.innerHeight;
-  var windowY = window.pageYOffset;
-  // console.log((windowY / scrollV) * 100);
-  bg_el.style.backgroundPosition = '0% ' + (windowY / scrollV) * 80 +　"%";
+  // var scrollH = document.documentElement.scrollHeight || document.body.scrollHeight;
+  // var scrollV = scrollH　- window.innerHeight;
+  // var windowY = window.pageYOffset;
+  
+  // var styleStr = 'translateX(' + (-windowY / scrollV) * 500 + 420 +　'px) ' + 'rotate(-26.5deg)';
+  // console.log(styleStr);
+  // containerEl.style.transform = styleStr;
 });
+
+var initial = document.getElementById("bg_triangle");
+var parent = document.getElementById("bg_target");
+
+for (var i = 0; i< 4; i++) {
+  for (var j = 0; j< 6; j++) {
+    var new_triangle = initial.cloneNode(false)
+    new_triangle.style.transform = 
+      "translateX(" + i * 800 + "px)" +
+      "translateY(" + j * 800 + "px)" ;
+    parent.appendChild(new_triangle);
+  }
+}
+console.log(initial, parent)
