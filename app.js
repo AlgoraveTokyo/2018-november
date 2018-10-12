9fb5f758deaa54f30f6e959c3796d740f8d3589b
moxuse <moxuse@gmail.com>
2018-10-12 13:44:53 +0900

    triangle bbackground

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 var containerEl = document.getElementById("container");
+var bgTargetEl =  document.getElementById("bg_target");
 
 window.addEventListener("scroll", function() {
-  // var scrollH = document.documentElement.scrollHeight || document.body.scrollHeight;
-  // var scrollV = scrollH　- window.innerHeight;
-  // var windowY = window.pageYOffset;
-  
-  // var styleStr = 'translateX(' + (-windowY / scrollV) * 500 + 420 +　'px) ' + 'rotate(-26.5deg)';
-  // console.log(styleStr);
-  // containerEl.style.transform = styleStr;
+  var scrollH = document.documentElement.scrollHeight || document.body.scrollHeight;
+  var scrollV = scrollH　- window.innerHeight;
+  var windowY = window.pageYOffset + 0;
+  var styleStr = 'translateX(' + (-1) * windowY + 'px)';
+  bgTargetEl.style.transform = styleStr;
 });
 
 var initial = document.getElementById("bg_triangle");
-var parent = document.getElementById("bg_target");
 
-for (var i = 0; i< 4; i++) {
-  for (var j = 0; j< 6; j++) {
+for (var i = 0; i< 10; i++) {
+  for (var j = 0; j< 10; j++) {
     var new_triangle = initial.cloneNode(false)
     new_triangle.style.transform = 
       "translateX(" + i * 800 + "px)" +
       "translateY(" + j * 800 + "px)" ;
-    parent.appendChild(new_triangle);
+      bgTargetEl.appendChild(new_triangle);
   }
 }
-console.log(initial, parent)
+
+function triangleWave(i) {
+  var m = 600;
+  return m - Math.abs((i % (2 * m) - m));
+}
